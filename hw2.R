8af0c4db02e7cdf30574ec03ddbf294e4a52b620
arkoneogy <a.s.neogy@gmail.com>
2015-07-17 18:06:17 +0530

    corrected hw2 code

--- hw2.R
+++ hw2.R
@@ -13,21 +13,28 @@ ss= training[,58:69]
 sx= testing[,58:69]
 
 s1= data.frame(ss, training$diagnosis)
-model= train(training.diagnosis ~ . , data= s1, method= 'glm')
-preds= predict(model, testing)
-confusionMatrix(preds, testing$diagnosis)
+model1= train(training.diagnosis ~ . , data= s1, method= 'glm')
+preds1= predict(model1, testing)
+confusionMatrix(preds1, testing$diagnosis)
 
 x= prcomp(ss, center = T, scale. = T)
 y= x$sdev ^2
 yy= y/sum(y)
 plot(cumsum(yy), type= 'l')
-z= prcomp(sx, center = T, scale. = T)
 
-s2= data.frame(x$x[,1:7], training$diagnosis)
-s3= data.frame(z$x[,1:7], testing$diagnosis)
-model= train(training.diagnosis ~ . , data= s2, method= 'glm')
-preds= predict(model, s3)
-confusionMatrix(preds, s3$testing.diagnosis)
+pcaModel= preProcess(ss, method= 'pca', pcaComp = 7)
+training.pca= data.frame(predict(pcaModel, ss), training$diagnosis)
+testing.pca= data.frame(predict(pcaModel, sx), testing$diagnosis)
+model2= train(training.diagnosis ~ . , data= training.pca, method= 'glm')
+preds2= predict(model2, testing.pca)
+confusionMatrix(preds2, testing.pca$testing.diagnosis)
+
+ctrl= trainControl(preProcOptions = list(thresh= 0.8))
+model3= train(training.diagnosis ~ . , data= s1, method= 'glm', preProcess= 'pca', trControl= ctrl)
+preds3= predict(model3, sx)
+confusionMatrix(preds3, testing$diagnosis)
+
+
 
 # q2
 data(concrete)
